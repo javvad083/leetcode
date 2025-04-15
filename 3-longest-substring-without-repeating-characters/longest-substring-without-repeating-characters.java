52a8ca520ef13ea8ed6ef04d6e906e7e75623427
javvad083 <132859852+javvad083@users.noreply.github.com>
2025-04-15 19:49:07 +0530

    Time: 2 ms (98.51%) | Memory: 42.7 MB (99.41%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -3,13 +3,13 @@ class Solution {
         int maxl=0;
         int[] fre=new int[128];
         int l=0;
-        //char lc=s.charAt(l);
         for(int r=0;r<s.length();++r){
-            //char cha=s.charAt(r);
-            ++fre[s.charAt(r)];
+            char cha=s.charAt(r);
+            ++fre[cha];
 
-            while(fre[s.charAt(r)]>1){
-                --fre[s.charAt(l)];
+            while(fre[cha]>1){
+                char lc=s.charAt(l);
+                --fre[lc];
                 l++;
             }
 
