7bafe00a788272522bff1df1adfbd267ac5a975f
javvad083 <132859852+javvad083@users.noreply.github.com>
2025-07-17 06:48:47 +0530

    Time: 204 ms (5.03%) | Memory: 45.3 MB (56.34%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,13 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n=nums.length;
-        int[] hash=new int[n+1];
-        for(int i=0;i<n;i++){
-            hash[nums[i]]=1;
+        int m=0;
+        for(int i=0;i<=n;i++){
+            int k=0;
+            for(int j=0;j<n;j++){
+                if(nums[j]==i){
+                    k=1;
+                    break;
+                }
+            }
+            if(k==0) m=i;
         }
-        for(int i=0;i<n+1;i++){
-            if(hash[i]==0) return i;
-        }
-        return 0;
+        return m;
     }
 }
