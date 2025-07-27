3f34b2bbc1ccbca1d80db7b6b9ec0687a7184f34
javvad083 <132859852+javvad083@users.noreply.github.com>
2025-07-27 20:59:12 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (40.92%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -2,7 +2,7 @@ class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
+            for(int j=0;j<i+1;j++){
                 int t=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=t;
@@ -11,8 +11,8 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<n/2;j++){
                 int t=matrix[i][j];
-                matrix[i][j]=matrix[i][n-1-j];
-                matrix[i][n-1-j]=t;
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=t;
             }
         }
     }
