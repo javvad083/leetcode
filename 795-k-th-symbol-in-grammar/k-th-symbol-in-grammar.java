208a821463cbd942a1e5dca2f5d06629e272b227
javvad083 <132859852+javvad083@users.noreply.github.com>
2025-11-18 15:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (8.58%) - LeetSync

--- 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
+++ 795-k-th-symbol-in-grammar/k-th-symbol-in-grammar.java
@@ -1,12 +1,11 @@
 class Solution {
     public int kthGrammar(int n, int k) {
-        if(n==1 && k==1){
+        if(n==1) {
             return 0;
         }
-        //System.out.print(n+" "+k+" | ");
-        int mp= 1<<(n-2);
+        int mp=1<<(n-2);
         if(k<=mp){
-        return kthGrammar(n-1,k);
+            return kthGrammar(n-1,k);
         }
         return kthGrammar(n-1,k-mp)^1;
     }
